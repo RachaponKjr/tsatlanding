b14f1ef329b2d0da5874bc15e7fe92ec670b00a6
RachaponKjr <rachapon.navy@gmail.com>
2025-09-10 15:15:38 +0700

    fix

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: 'http://150.95.26.51:3031',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
     credentials: true,
   }),
 );
